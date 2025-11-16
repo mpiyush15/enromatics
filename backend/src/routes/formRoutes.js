473cbbe2fe48404ce5c00e87cebc77bde50e2227
mpiyush15 <piyush@pixelsdigital.tech>
2025-11-16 16:48:05 +0530

    error n sub file

--- backend/src/routes/formRoutes.js
+++ backend/src/routes/formRoutes.js
@@ -1,12 +1,12 @@
 import express from "express";
-import Formschema from "../models/Subscriber.js";
+import Subscriber from "../models/Subscriber.js";
 
 const router = express.Router();
 
 router.post("/submit", async (req, res) => {
   try {
     const formData = req.body;
-    const savedData = await Form.create(formData);
+    const savedData = await Subscriber.create(formData);
     res.status(201).json({ message: "Form data saved successfully", data: savedData });
   } catch (err) {
     console.error(err);
